415c0742d44fb2fffe8ca94de79ec5bbf8e2dc9e
TourretAlexandre <alexandre.tourret77@gmail.com>
2020-10-25 17:22:59 +0100

    oui

--- lib/login.dart
+++ lib/login.dart
@@ -18,13 +18,17 @@ class Body extends StatefulWidget {
 class _BodyState extends State<Body> {
   FirebaseUser user;
 
+  @override
+  void initState() {
+    super.initState();
+    signOutGoogle();
+  }
+
   void click() {
     signInWithGoogle().then((user) => {
           this.user = user,
-          Navigator.push(
-              context,
-              MaterialPageRoute(
-                  builder: (context) => MyHomePage(user.displayName)))
+          Navigator.push(context,
+              MaterialPageRoute(builder: (context) => MyHomePage(user)))
         });
   }
 
